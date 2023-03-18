4d43dc31191cad1865ae066a2f1fbb202d6f69e5
vinayakmahindrakar <mahindrakar.project@gmail.com>
2023-03-18 18:07:55 +0530

    setup gh-pages

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -4,7 +4,7 @@ const VideoCard = ({ info }) => {
   const { snippet, statistics } = info;
   const { channelTitle, title, thumbnails } = snippet;
   return (
-    <div className="p-2 m-2 w-72 shadow-lg">
+    <div className="p-2 m-2 w-64 shadow-lg">
       <img className="rounded-lg" alt="thumbnail" src={thumbnails.medium.url} />
       <ul>
         <li className="font-bold py-2">{title}</li>
---
+++ src/components/WatchPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WatchPage = () => {
+  return <div>i am watch</div>;
+};
+
+export default WatchPage;
